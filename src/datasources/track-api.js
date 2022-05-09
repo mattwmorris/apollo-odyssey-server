6af7f2976234b856643b463f59aa9f8654817493
mattwmorris <matthewwarrenmorris@gmail.com>
2022-05-09 09:20:52 -0700

    fixing cert error

--- src/datasources/track-api.js
+++ src/datasources/track-api.js
@@ -1,9 +1,11 @@
+// src/datasources/track-api.js
 const { RESTDataSource } = require('apollo-datasource-rest');
+const { HttpsProxyAgent } = require('https-proxy-agent');
+const https = require('https');
 
 class TrackAPI extends RESTDataSource {
   constructor() {
     super();
-    // the Catstronauts catalog is hosted on this server
     this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
   }
 
@@ -15,21 +17,19 @@ class TrackAPI extends RESTDataSource {
     return this.get(`author/${authorId}`);
   }
 
+  willSendRequest(request) {
+    request.agent = new https.Agent({ rejectUnauthorized: false })
+  }
   getTrack(trackId) {
     return this.get(`track/${trackId}`);
-  }
-
+  }  
   getTrackModules(trackId) {
     return this.get(`track/${trackId}/modules`);
   }
-
-  getModule(moduleId) {
-    return this.get(`module/${moduleId}`);
-  }
-
   incrementTrackViews(trackId) {
     return this.patch(`track/${trackId}/numberOfViews`);
-  }
+ }
 }
+
 
 module.exports = TrackAPI;
